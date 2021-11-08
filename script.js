2a29602f6ac4a51953f28b17fa3669069c2c7e87
robonexx <robertwagar@gmail.com>
2021-11-08 15:05:36 +0100

    color picker function

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEl = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
 const ctx = canvas.getContext('2d');
 
 let size = 20;
@@ -17,13 +22,13 @@ canvas.addEventListener('mousedown', (e) => {
 canvas.addEventListener('mousemove', (e) => {
   if (isPressed) {
     const x2 = e.offsetX;
-      const y2 = e.offsetY;
-      
-      drawCircle(x2, y2);
-      drawLine(x, y, x2, y2)
+    const y2 = e.offsetY;
 
-      x = x2
-      y = y2
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    x = x2;
+    y = y2;
   }
 });
 
@@ -50,5 +55,4 @@ function drawLine(x1, y1, x2, y2) {
   ctx.stroke();
 }
 
-drawCircle();
-drawLine();
+colorEl.addEventListener('change', (e) => (color = e.target.value));
